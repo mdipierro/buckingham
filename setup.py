fe38e2c86bba31ca464b9321ee092031561475df
mdipierro <massimo.dipierro@gmail.com>
2019-05-25 00:14:49 -0700

    0.3

--- setup.py
+++ setup.py
@@ -1,16 +1,37 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-from setuptools import setup
+import ast
+import re
 
-setup(name='buckingham',
-      version='0.1',
-      description='A library for unit conversion and error propagation',
-      author='Massimo DiPierro',
-      author_email='massimo.dipierro@gmail.com',
-      long_description=open("README.md").read(),
-      url='https://github.com/mdipierro/buckingham',
-      install_requires=[],
-      py_modules=["buckingham"],
-      license= 'BSD',
-      keywords='unit conversion and error propagation',
-      )
+import setuptools
+
+setuptools.setup(
+    name = "buckingham",
+    version = 0.3,
+    url = 'https://github.com/mdipierro/buckingham',
+    license = 'BSD',
+    author = 'Massimo Di Pierro',
+    author_email = 'massimo.dipierro@gmail.com',
+    maintainer = 'Massimo Di Pierro',
+    maintainer_email = 'massimo.dipierro@gmail.com',
+    description = 'a library for unit conversion and error propagation',
+    packages = ['buckingham'],
+    include_package_data = True,
+    zip_safe = False,
+    platforms = 'any',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
